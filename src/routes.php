f0c4b5457c230ef13e77d89e60e574c494da444f
botflux <victor.mendele68@gmail.com>
2019-05-27 18:54:42 +0200

    :star2:feat: add a new route

--- src/routes.php
+++ src/routes.php
@@ -4,8 +4,11 @@ use App\Router\Request;
 use App\Router\Response;
 
 $app->get('/^\/$/', function (Request $request, Response $response) use ($app) {
-
     return $app->render('index.phtml', [
         'message' => 'hello world'
     ]);
+});
+
+$app->get('/^\/about$/', function (Request $request, Response $response) use ($app) {
+    return $app->render('about.phtml', []);
 });

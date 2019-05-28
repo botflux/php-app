8878fbba3f2c31c2cc12a02ca9b5f1a7fcab6fb1
botflux <victor.mendele68@gmail.com>
2019-05-28 18:05:35 +0200

    :star2:feat: pdo service

--- config/config.php
+++ config/config.php
@@ -5,5 +5,12 @@ $projectRoot = __DIR__ . '/..';
 return [
     'renderer' => [
         'templateFolder' => "$projectRoot/template"
+    ],
+    'database' => [
+        'host' => 'localhost',
+        'port' => 3306,
+        'username' => 'root',
+        'password' => '',
+        'base' => 'php-app'
     ]
 ];
--- src/services.php
+++ src/services.php
@@ -5,3 +5,8 @@ $container = $app->getContainer();
 $container['renderer'] = function ($c) {
     return new App\Renderer\Renderer($c['settings']['renderer']['templateFolder']);
 };
+
+$container['pdo'] = function ($c) {
+    $settings = $c['settings']['database'];
+    return new PDO("mysql:dbname={$settings['base']};host={$settings['host']}", $settings['username'], $settings['password']);
+};

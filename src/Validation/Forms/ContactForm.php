1913decb131756d65c7cb27482042e7e10ba22d1
botflux <victor.mendele68@gmail.com>
2019-06-08 18:00:56 +0200

    :star2:feat: add contact form

---
+++ src/Validation/Forms/ContactForm.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Validation\Forms;
+
+use App\Validation\BaseForm;
+use App\Validation\FormField;
+use App\Validation\Types\EmailType;
+use App\Validation\Types\Required;
+use App\Validation\Types\Phone;
+use App\Validation\Types\Length;
+
+class ContactForm extends BaseForm 
+{
+    public function __construct()
+    {
+        $this
+            ->add(new FormField(
+                'email', 
+                [
+                    new Required(),
+                    new EmailType(),
+                ]
+            ))
+            ->add(new FormField(
+                'name',
+                [
+                    new Required()
+                ]
+            ))
+            ->add(new FormField(
+                'phone',
+                [
+                    new Required(),
+                    new Phone()
+                ]
+            ))
+            ->add(new FormField(
+                'content',
+                [
+                    new Required(),
+                    new Length(5, 300)
+                ]
+            ))
+        ;
+    }
+}

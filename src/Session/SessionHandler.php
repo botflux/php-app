bd0bd67171345c8b49a05e75c13040a87b924bb7
botflux <victor.mendele68@gmail.com>
2019-06-08 18:01:12 +0200

    :star2:feat: add session handler

---
+++ src/Session/SessionHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Session;
+
+/**
+ * Session handler
+ */
+class SessionHandler 
+{
+    /**
+     * Set session array
+     *
+     * @param array $newSessionArray
+     * @return self
+     */
+    public function set (array $newSessionArray): self {
+        $_SESSION = $newSessionArray;
+
+        return $this;
+    }
+
+    /**
+     * Get session array
+     *
+     * @return array
+     */
+    public function get (): array {
+        return $_SESSION;
+    }
+}
